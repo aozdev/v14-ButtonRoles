8e794e3e6be86796d0afabc5c774e6238507df5b
arda <aoztrk009@gmail.com>
2026-01-23 03:26:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+const { 
+  Client, 
+  GatewayIntentBits, 
+  Collection,
+  REST,
+  Routes
+} = require("discord.js");
 
+const fs = require("fs");
+const path = require("path");
+const config = require("./config.json");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.commands = new Collection();
+
+async function deployCommands() {
+  const commands = [];
+  const commandsPath = path.join(__dirname, "commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    if (command.data) {
+      commands.push(command.data.toJSON());
+    }
+  }
+
+  const rest = new REST({ version: "10" }).setToken(config.token);
+
+  try {
+    console.log("Started refreshing global application (/) commands.");
+
+    await rest.put(
+      Routes.applicationCommands(config.clientId),
+      { body: commands }
+    );
+
+    console.log("🚀 Successfully reloaded global application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+require("./handlers/commandHandler")(client);
+require("./handlers/eventHandler")(client);
+
+(async () => {
+  await deployCommands();
+  await client.login(config.token);
+})();
